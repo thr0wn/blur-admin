663dfc38e6f0347772aae7dcfb3c63dc7e5352b1
Natan Camargos <natanscamargos@gmail.com>
2016-04-12 22:51:59 -0300

    remove revision number of release files

--- gulp/build.js
+++ gulp/build.js
@@ -41,7 +41,7 @@ gulp.task('html', ['inject', 'partials'], function () {
   return gulp.src(path.join(conf.paths.tmp, '/serve/*.html'))
     .pipe($.inject(partialsInjectFile, partialsInjectOptions))
     .pipe(assets = $.useref.assets())
-    .pipe($.rev())
+    // .pipe($.rev())
     .pipe(jsFilter)
     .pipe($.sourcemaps.init())
     .pipe($.ngAnnotate())
